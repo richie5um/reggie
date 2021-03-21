f7ea773f61d1e6436a60850e2e9fbfb677b1c790
Rich Somerfield <rs@richsomerfield.com>
2021-03-21 17:37:02 +0000

    loadsa improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,12 @@ export class AppComponent {
   public input: string = 'hello world\nhello\nzzz';
   public replaces: any[] = [];
   public matches: any[] = [];
-  private lastAction: string;
+  private lastAction: string = 'match';
+
+  public matchshow: boolean = true;
+  public replaceshow: boolean = true;
+  public inputshow: boolean = true;
+  public outputshow: boolean = true;
 
   public matchhelp: boolean = false;
   public replacehelp: boolean = false;
@@ -27,7 +32,7 @@ export class AppComponent {
 
   ngOnInit() {
     this.load();
-    this.match();
+    this.runLastAction();
   }
 
   load() {
@@ -42,6 +47,9 @@ export class AppComponent {
 
     const input = this.localStorageService.getItem('re-input');
     this.input = input ? input : this.input;
+
+    const lastaction = this.localStorageService.getItem('re-lastaction');
+    this.lastAction = lastaction ? lastaction : this.lastAction;
   }
 
   save() {
@@ -49,15 +57,18 @@ export class AppComponent {
     this.localStorageService.setItem('re-options', this.regexoptions);
     this.localStorageService.setItem('re-replace', this.regexreplace);
     this.localStorageService.setItem('re-input', this.input);
+    this.localStorageService.setItem('re-lastaction', this.lastAction);
   }
 
   runLastAction() {
     switch (this.lastAction) {
       case 'replace':
         this.replace();
+        break;
       case 'match':
       default:
         this.match();
+        break;
     }
   }
 
@@ -66,7 +77,10 @@ export class AppComponent {
     this.matches = undefined;
     this.replaces = undefined;
 
-    const regex = new RegExp(this.regexmatch, this.regexoptions);
+    this.regexmatch = RegexUtil.replaceAll(this.regexmatch, ' ', '·');
+    const regexmatch = RegexUtil.replaceAll(this.regexmatch, '·', ' ');
+
+    const regex = new RegExp(regexmatch, this.regexoptions);
 
     let matches = [];
     const inputs = this.input.split('\n');
@@ -110,12 +124,18 @@ export class AppComponent {
     this.matches = undefined;
     this.replaces = undefined;
 
-    const regex = new RegExp(this.regexmatch, this.regexoptions);
+    this.regexreplace = RegexUtil.replaceAll(this.regexreplace, ' ', '·');
+    const regexreplace = RegexUtil.replaceAll(this.regexreplace, '·', ' ');
+
+    this.regexmatch = RegexUtil.replaceAll(this.regexmatch, ' ', '·');
+    const regexmatch = RegexUtil.replaceAll(this.regexmatch, '·', ' ');
+
+    const regex = new RegExp(regexmatch, this.regexoptions);
 
     let replaces = [];
     const inputs = this.input.split('\n');
     for (const input of inputs) {
-      const replace = input.replace(regex, this.regexreplace);
+      const replace = input.replace(regex, regexreplace);
       replaces = replaces.concat({ input: input, output: replace });
     }
 
